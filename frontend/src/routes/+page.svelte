f2d9282aa3ce2c6813eb9d40fd55fb51ef7dbdf1
leoBravo <leo.bravo.rain@gmail.com>
2025-08-12 21:37:21 -0300

    feat: implement basic API connection

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	
+	const API_BASE_URL = 'http://localhost:8000';
+	
+    // TODO: separate responsabilities (creating a service?)
+	// Simple function to test backend connection
+	async function testBackendConnection() {
+		try {
+            // TODO: manage routes as constants
+			const response = await fetch(`${API_BASE_URL}/api/files`);  
+			
+			if (response.ok) {
+				const data = await response.json();
+                console.log(data);
+			}
+		} catch (err) {
+			console.error('Error connecting to backend:', err);
+		}
+	}
+	
+	onMount(() => {
+		testBackendConnection();
+	});
+</script>
+
+<div class="p-8">
+	<h1 class="text-2xl font-bold mb-4">Google Drive Clone - Frontend</h1>
+</div>
