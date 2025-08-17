70340db489fe64bd5754195fac959fdfc77a265a
leoBravo <leo.bravo.rain@gmail.com>
2025-08-17 13:14:52 -0300

    feat: update indexes DB

--- data/mongo-init/init.js
+++ data/mongo-init/init.js
@@ -8,11 +8,10 @@ db = db.getSiblingDB('filedrive');
 db.createCollection('files');
 
 // Create indexes for better performance
-db.files.createIndex({ "filename": 1 });
 db.files.createIndex({ "upload_date": -1 });
-db.files.createIndex({ "file_type": 1 });
+db.files.createIndex({ "file_id": 1 });
 
 print("MongoDB initialization completed successfully!");
 print("Database: filedrive");
 print("Collection: files");
-print("Indexes created for: filename, upload_date, file_type, user_id");
+print("Indexes created for:upload_date, file_id");
