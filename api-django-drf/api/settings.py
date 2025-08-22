d501d3d6743d677655aff873a8c4a10b82e3db4d
leoBravo <leo.bravo.rain@gmail.com>
2025-08-22 14:04:23 -0300

    feat: create basic files app

--- api-django-drf/api/settings.py
+++ api-django-drf/api/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'api.files'
 ]
 
 MIDDLEWARE = [
