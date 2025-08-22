2ceaf804fe0501f05b8cca0b43b01ba988b75c83
leoBravo <leo.bravo.rain@gmail.com>
2025-08-22 14:31:33 -0300

    feat: add basic get endpoint

--- api-django-drf/api/files/views.py
+++ api-django-drf/api/files/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def hello(req):
+    """Simple endpoint to return hello"""
+    return Response("Hello, World!")
