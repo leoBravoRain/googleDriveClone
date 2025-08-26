1b83780f3a98d2bcfb5ab2cd23f9bcfce59baa2b
leoBravo <leo.bravo.rain@gmail.com>
2025-08-26 12:14:31 -0300

    feat: add more fields to entity

--- nest-graphql/src/files/entities/file.entity.ts
+++ nest-graphql/src/files/entities/file.entity.ts
@@ -3,8 +3,35 @@ import { ObjectType, Field } from '@nestjs/graphql';
 @ObjectType()
 export class File {
   @Field(() => String)
+  _id: string;
+
+  @Field(() => String)
   file_id: string;
 
   @Field(() => String)
   filename: string;
+
+  @Field(() => String)
+  original_name: string;
+
+  @Field(() => String)
+  size: string;
+
+  @Field(() => String)
+  file_type: string;
+
+  @Field(() => String)
+  updated_date: string;
+
+  @Field(() => String)
+  storage_path: string;
+
+  @Field(() => String, { nullable: true })
+  user_id: string;
+
+  @Field(() => String)
+  created_at: string;
+
+  @Field(() => String)
+  updated_at: string;
 }
