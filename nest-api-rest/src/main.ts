c43069b48e8b6c3d1a312680804857c988f26e10
leoBravo <leo.bravo.rain@gmail.com>
2025-08-27 10:37:55 -0300

    feat: add swagger basic configuratino

--- nest-api-rest/src/main.ts
+++ nest-api-rest/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Nest-API-REST')
+    .setDescription('API documentation for this demo api-rest')
+    .setVersion('1.0')
+    .build();
+
+  // Create Swagger doc
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
