aa6ca580e2bbfc70296fed04d0bb0c17faed9d72
leoBravo <leo.bravo.rain@gmail.com>
2025-08-27 10:21:59 -0300

    fix: fix PORT for nest-api-rest app

--- nest-api-rest/src/app.module.ts
+++ nest-api-rest/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FilesModule } from './files/files.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FilesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    FilesModule,
+  ],
   controllers: [],
   providers: [],
 })
