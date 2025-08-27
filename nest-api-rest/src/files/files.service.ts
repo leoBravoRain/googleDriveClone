40f11b2804c51fa4be85e93873d6f136134db9a9
leoBravo <leo.bravo.rain@gmail.com>
2025-08-27 10:25:37 -0300

    feat: add fetch request to other service

--- nest-api-rest/src/files/files.service.ts
+++ nest-api-rest/src/files/files.service.ts
@@ -4,13 +4,12 @@ import { File } from './entities/file.entity';
 @Injectable()
 export class FilesService {
   async findAll(): Promise<File[]> {
-    // // TODO: move to separate service and define axios object to manage API Rest calls
-    // // communicate with backend service
-    // const response = await fetch(process.env.API_FILES_URL || '');
+    // TODO: move to separate service and define axios object to manage API Rest calls
+    // communicate with backend service
+    const response = await fetch(process.env.API_FILES_URL || '');
 
-    // const data = (await response.json()) as { files: File[] };
+    const data = (await response.json()) as { files: File[] };
 
-    // return data.files;
-    return [];
+    return data.files;
   }
 }
