58c322b4e0be26884974c024837cb1ae853b5194
leoBravo <leo.bravo.rain@gmail.com>
2025-08-27 10:01:15 -0300

    feat(nest-api-rest): create files module

---
+++ nest-api-rest/src/files/entities/file.entity.ts
@@ -0,0 +1,13 @@
+export class File {
+    id: string;
+    file_id: string;
+    filename: string;
+    original_name: string;
+    size: string;
+    file_type: string;
+    updated_date: string;
+    storage_path: string;
+    user_id: string;
+    created_at: string;
+    updated_at: string;
+}
---
+++ nest-api-rest/src/files/files.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { File } from './entities/file.entity';
+
+@Injectable()
+export class FilesService {
+  async findAll(): Promise<File[]> {
+    // // TODO: move to separate service and define axios object to manage API Rest calls
+    // // communicate with backend service
+    // const response = await fetch(process.env.API_FILES_URL || '');
+
+    // const data = (await response.json()) as { files: File[] };
+
+    // return data.files;
+    return [];
+  }
+}
