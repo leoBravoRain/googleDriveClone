1b92333d2de942d15bad68074fd83b47b6a03a4a
leoBravo <leo.bravo.rain@gmail.com>
2025-09-02 18:24:20 -0300

    feat: add hash and sets

---
+++ dataAndAlgorithmPractice/hashAndSets.ts
@@ -0,0 +1,76 @@
+function assertEquals(actual: any, expected: any, testName: string) {
+    if(JSON.stringify(actual) === JSON.stringify(expected)){
+        console.log(`Test ${testName} passed ✅`);
+    }
+    else {
+        console.log(`Test ${testName} failed ❌`);
+    }
+}
+
+const map = new Map<string, number>();
+
+map.set('apple', 2);
+map.set('apple2', 2);
+
+for(const [key, value] of map) {
+    console.log(key, value)
+}
+
+function countCharacters(word: string): Map<string, number> {
+
+    let counter = new Map<string, number>();
+
+    for(let i = 0; i<word.length; ++i) {
+        let char = word[i];
+
+        console.log(char);
+
+        if(!(char in counter)) {
+            // counter.set(char, 0);
+            counter[char] = 0;
+        }
+
+        // counter.set(char, (counter.get(char) ?? 0) + 1);
+        counter[char] = counter[char] + 1;
+
+    }
+
+    return counter;
+}
+
+function twoSumHashMap(nums: number[], target: number) {
+
+    let map = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; ++i) {
+        let compliment = target - nums[i];
+
+        // if compliment exists in map, so the number (compliment) was found
+        if(map.has(compliment)) {
+
+            // return compliement's index, index
+            return [map.get(compliment), i];
+        }
+
+        // if not found, then add value -> index (to check if it can be useful by other number in the next iterarions)
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+// test for two sum
+console.log(assertEquals(twoSumHashMap([1, 3, 6, 2], 5), [1, 3], 'should return expected when target is 5'));
+
+
+const set = new Set<string>;
+
+set.add('apple');
+set.add('pineApple');
+
+console.log(set);
+
+console.log('counting characteres: ', JSON.stringify(countCharacters('eliana')));
+
+for(let value of set){
+    console.log(value);
+}
